7a869e2dd8365909157d0e8178b28ea0f03154c4
FranBenthien <113780593+FranBenthien@users.noreply.github.com>
2022-09-17 11:52:37 -0300

    Criado o login

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,32 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using static System.IO.File;
 
 namespace meuprimeiroprojeto.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+[Route("/")]
+public class TestController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+   [HttpGet("oi")]
+   public ActionResult OutroTest ()
+   {
+        return Content(
+        ReadAllText("views/index.html"), "text/html");
+   }
 
-    private readonly ILogger<WeatherForecastController> _logger;
+   [HttpGet("ola")]
+   public ActionResult OutroOutroTest ()
+   {
+        return Content(
+        ReadAllText("views/login.html"), "text/html");
+   }
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
+   [HttpGet]
+   public string Test ()
+   {
+        return "Meu Backend está rodando";
+
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
 }
+
+
